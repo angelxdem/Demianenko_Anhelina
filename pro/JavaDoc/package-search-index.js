42ae28146e7532b1f7ce026adce9af817bb3ab5b
angelxdem <113301385+angelxdem@users.noreply.github.com>
2023-03-08 12:24:20 +0200

    Add files via upload

--- pro/JavaDoc/package-search-index.js
+++ pro/JavaDoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"ex01","l":"ex01"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"ex01","l":"ex01"},{"m":"ex01","l":"ex02"}];updateSearchResults();
